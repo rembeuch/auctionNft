28a14f5ae8dca487faf92764c96b46f2dfff91d7
rem beuch <beucherie.r@gmail.com>
2023-08-30 10:35:50 +0200

    sepolia in app js provider

--- frontend2/pages/_app.js
+++ frontend2/pages/_app.js
@@ -6,13 +6,13 @@ import {
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { hardhat } from 'wagmi/chains';
+import { hardhat, sepolia } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 import { ChakraProvider } from '@chakra-ui/react'
 
 const { chains, provider } = configureChains(
-  [hardhat],
+  [hardhat, sepolia],
   [
     //alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
     publicProvider()
